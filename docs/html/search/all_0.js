c0a6fe8981c68c52e5b74e7d4ebcf7acf95fcee0
firsou <guschefi@student.gu.se>
2018-04-24 23:38:59 +0200

    Added HTML and LaTeX documentation

---
+++ docs/html/search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['announcepresence',['announcePresence',['../class_v2_v_service.html#a1e2f575ac83bebcda2c557d425b3050f',1,'V2VService']]]
+];
---
+++ docs/html/search/all_3.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['ps4event',['PS4Event',['../struct_p_s4_event.html',1,'']]]
+];
