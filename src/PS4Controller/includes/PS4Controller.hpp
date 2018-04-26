61eedfc7ee34ac4206c44c31df2afb388a64ba2a
firsou <guschefi@student.gu.se>
2018-04-26 22:29:31 +0200

    Modified deceleration value to meet ESC specifications

--- src/PS4Controller/includes/PS4Controller.hpp
+++ src/PS4Controller/includes/PS4Controller.hpp
@@ -10,7 +10,7 @@
 static constexpr float MIN_AXES_VALUE = -32768.0f;      /**< Minimum value of axes range*/
 static constexpr float MAX_AXES_VALUE = 32767.0f;       /**< Maximum value of axes range.*/
 
-static constexpr float m_MAX_DECELERATION = -0.35;    /**< Max Deceleration value, must be negative.*/
+static constexpr float m_MAX_DECELERATION = -1.0;    /**< Max Deceleration value, must be negative.*/
 static constexpr float m_MAX_ACCELERATION = 0.25;       /**< Max Acceleration value, must be positive.*/
 static constexpr float m_MAX_STEERING_ANGLE = 38.0f;     /**< Max Steering Angle, must be positive.*/
 
