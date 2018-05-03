1f74a1747394a8bb81fd62d982d54f7fedfaab22
omidmanai <gusmanom@student.gu.se>
2018-05-03 11:31:20 +0200

    The old Signal Viewer is added with its js code and the button toggles its presence now

--- src/opendlv-signal-viewer/src/libcluon-viewer-0.01.js
+++ src/opendlv-signal-viewer/src/libcluon-viewer-0.01.js
@@ -158,13 +158,51 @@ if ("WebSocket" in window) {
             $("#apList").append('<li>' + " Follower Status Received! " + '</li>');
         }
 
+        onMessageReceived(__libcluon, evt.data);
+
+        // if(document.getElementById('goodOldSignal').visibility === "visible") {
+            console.log(document.getElementById('goodOldSignal').visibility);
+            document.getElementById('goodOldSignal').onclick = function (e) {
+                alert('hello');
+            }
+        // }
+
     };
     ws.onclose = function() {
         console.log("Connection is closed.");
     };
+
+    // let oldSignal = document.getElementById('goodOldSignal').visibility;
+    // let signalButton = document.getElementById('graphButton');
+    //
+    // if(oldSignal === "hidden"){
+    //     signalButton.addEventListener('click', function () {
+    //         oldSignal = "visible";
+    //     })
+    // }
+    // else if(oldSignal === "visible"){
+    //     signalButton.addEventListener('click', function () {
+    //         oldSignal = "hidden";
+    //     })
+    // }
+
+
+
+    $(document).ready(function() {
+
+        $("#graphButton").on('click', function () {
+            $("#goodOldSignal").toggle();
+        });
+
+        createDashboard();
+        $(document).bind("kendo:skinChange", function(e) {
+            createDashboard();
+        });
+
+    });
 }
 else {
-    console.log("WebSocket is not supported by your Browserb!");
+    console.log("WebSocket is not supported by your Browser!");
 }
 
 /*
@@ -326,14 +364,322 @@ function createDashboard() {
 }
 
 
+const g_renderFreq = 4.0;
+const g_maxDataAge = 10.0;
+const g_maxFieldChartValues = 10;
 
-$(document).ready(function() {
+var g_charts = new Map();
+var g_chartConfigs = new Map();
+var g_data = new Map();
+var g_pause = false;
 
-    createDashboard();
+setInterval(onInterval, Math.round(1000 / g_renderFreq));
+
+function onMessageReceived(lc, msg) {
+
+    if (g_pause) {
+        return;
+    }
+
+    var data_str = lc.decodeEnvelopeToJSON(msg);
+
+    if (data_str.length === 2) {
+        return;
+    }
+
+    d = JSON.parse(data_str);
 
 
-    $(document).bind("kendo:skinChange", function(e) {
-        createDashboard();
+    // Translate to nice JSON ..
+    var payloadFields = new Array();
+
+    const payloadName = Object.keys(d)[5];
+
+    for (const fieldName in d[payloadName]) {
+        const fieldValue = d[payloadName][fieldName];
+        const field = {
+            name : fieldName,
+            value : fieldValue,
+            type : (typeof fieldValue)
+        };
+        payloadFields.push(field);
+    }
+
+    const data = {
+        dataType : d.dataType,
+        payload : {
+            name : payloadName,
+            fields : payloadFields
+        },
+        received : d.received,
+        sampleTimeStamp : d.sampleTimeStamp,
+        senderStamp : d.senderStamp,
+        sent : d.sent
+    };
+    // .. done.
+
+    const sourceKey = data.dataType + '_' + data.senderStamp;
+    const dataSourceIsKnown = g_data.has(sourceKey);
+
+    if (!dataSourceIsKnown) {
+        addTableData(sourceKey, data);
+        addFieldCharts(sourceKey, data);
+
+        g_data.set(sourceKey, new Array());
+    }
+
+    storeData(sourceKey, data);
+}
+
+function toTime(t) {
+    const milliseconds = t.seconds * 1000 + t.microseconds / 1000;
+    return moment(milliseconds).format('YYYY-MM-DD hh:mm:ss');
+}
+
+function cutLongField(type, value) {
+    if (type === 'string' && value.length > 20) {
+        value = value.substr(0, 20) + ' <span class="dots">[...]</span>';
+    }
+    return value;
+}
+
+function addTableData(sourceKey, data) {
+
+    if($('tr#' + sourceKey).length === 0) {
+
+        const name = data.payload.name;
+        const type = data.dataType;
+        const sender = data.senderStamp;
+        const timestamp = toTime(data.sampleTimeStamp);
+
+        const headerHtml = '<tr id="' + sourceKey + '" class="dataHeader"><td>'
+            + type + '</td><td>' + sender + '</td><td>' + name + '</td><td id="'
+            + sourceKey + '_frequency">N/A</td><td id="'
+            + sourceKey + '_timestamp">' + timestamp + '</td></tr>';
+
+        const fieldCount = data.payload.fields.length;
+
+        var fieldsHtml = '<tr id="' + sourceKey + '_fields" class="hidden">'
+            + '<td colspan="6"><table class="dataFields">';
+        for (var i = 0; i < fieldCount; i++) {
+
+            const field = data.payload.fields[i];
+            const fieldName = field.name;
+            const fieldValue = cutLongField(field.type, field.value);
+
+            fieldsHtml += '<tr><td class="field-name">' + fieldName + '</td>'
+                + '<td class="field-plot"><canvas id="' + sourceKey + '_field'
+                + i + '_canvas"></canvas></td><td id="' + sourceKey + '_field'
+                + i + '_value" class="field-value">' + fieldValue + '</td></tr>';
+        }
+
+        fieldsHtml += '</td></tr></table>';
+
+        $('#dataView > tbody:last-child').append(headerHtml);
+        $('#dataView > tbody:last-child').append(fieldsHtml);
+    }
+}
+
+function addFieldCharts(sourceKey, data) {
+
+    const fieldCount = data.payload.fields.length;
+
+    for (var i = 0; i < fieldCount; i++) {
+
+        const field = data.payload.fields[i];
+        const fieldType = field.type;
+
+        if (fieldType === 'number') {
+
+            const fieldKey = sourceKey + '_' + i;
+
+            const fieldName = field.name;
+            const fieldValue = field.value;
+
+            const config = {
+                type: 'line',
+                data: {
+                    labels: [0.0],
+                    datasets: [{
+                        label: fieldName,
+                        data: [fieldValue],
+                        backgroundColor: 'rgb(255, 99, 132)',
+                        borderColor: 'rgb(255, 99, 132)',
+                        fill: false
+                    }]
+                },
+                options: {
+                    responsive: true,
+                    title: {
+                        display: false,
+                    },
+                    tooltips: {
+                        mode: 'index',
+                        intersect: false,
+                    },
+                    hover: {
+                        mode: 'nearest',
+                        intersect: true
+                    },
+                    legend: {
+                        display: false
+                    },
+                    animation: {
+                        duration: 0
+                    },
+                    scales: {
+                        xAxes: [{
+                            display: true,
+                            scaleLabel: {
+                                display: true,
+                                labelString: 'time'
+                            }
+                        }],
+                        yAxes: [{
+                            display: true,
+                            scaleLabel: {
+                                display: true,
+                                labelString: fieldName
+                            }
+                        }]
+                    }
+                }
+            };
+
+            var ctx = document.getElementById(sourceKey + '_field' + i + '_canvas').getContext('2d');
+            var chart = new Chart(ctx, config);
+
+            g_charts[fieldKey] = chart;
+            g_chartConfigs[fieldKey] = config;
+        }
+    }
+}
+
+function storeData(sourceKey, data) {
+
+    const newDataTime = data.sent.seconds;
+    const dataList = g_data.get(sourceKey);
+
+    var dataPointsToShift = 0;
+    for (var i = 0; i < dataList.length; i++) {
+        const prevData = dataList[i];
+        const dataTime = prevData.sent.seconds;
+        const age = newDataTime - dataTime;
+
+        if (age > g_maxDataAge) {
+            dataPointsToShift++;
+        } else {
+            break;
+        }
+    }
+
+    for (var i = 0; i < dataPointsToShift; i++) {
+        g_data.get(sourceKey).shift();
+    }
+    g_data.get(sourceKey).push(data);
+}
+
+function onInterval() {
+    if (g_pause) {
+        return;
+    }
+
+    g_data.forEach(function(dataList, sourceKey, map) {
+        const newestData = dataList[dataList.length - 1];
+        updateTableData(sourceKey, newestData);
+        updateFieldCharts(sourceKey, dataList);
     });
+}
 
-});
+function updateTableData(sourceKey, data) {
+
+    const dataList = g_data.get(sourceKey);
+    if (dataList.length > 10) {
+        const firstTimestamp = dataList[0].sent;
+        const firstTime = firstTimestamp.seconds * 1000 + firstTimestamp.microseconds / 1000;
+        const lastTimestamp = dataList[dataList.length - 1].sent;
+        const lastTime = lastTimestamp.seconds * 1000 + lastTimestamp.microseconds / 1000;
+        const frequency = Math.round(1000 * dataList.length / (lastTime - firstTime));
+        $('td#' + sourceKey + '_frequency').html(frequency);
+    } else {
+        $('td#' + sourceKey + '_frequency').html('N/A');
+    }
+
+    const timestamp = toTime(data.sampleTimeStamp);
+    $('td#' + sourceKey + '_timestamp').html(timestamp);
+
+    const fieldCount = data.payload.fields.length;
+    for (var i = 0; i < fieldCount; i++) {
+        const field = data.payload.fields[i];
+        const fieldValue = cutLongField(field.type, field.value);
+        $('td#' + sourceKey + '_field' + i + '_value').html(fieldValue);
+    }
+}
+
+function updateFieldCharts(sourceKey, dataList) {
+
+    const latestData = dataList[dataList.length - 1];
+    const latestTimestamp = latestData.sent;
+    const latestTime = latestTimestamp.seconds * 1000 + latestTimestamp.microseconds / 1000;
+
+    const fieldCount = latestData.payload.fields.length;
+
+    // Clear charts.
+    for (var i = 0; i < fieldCount; i++) {
+        const field = latestData.payload.fields[i];
+        const fieldType = field.type;
+        if (fieldType === 'number') {
+            const fieldKey = sourceKey + '_' + i;
+            const config = g_chartConfigs[fieldKey];
+            config.data.labels = new Array();
+            config.data.datasets[0].data = new Array();
+        }
+    }
+
+    // Add new data.
+    const dataCount = dataList.length;
+    const decimation = Math.floor(dataCount / (g_maxFieldChartValues - 1));
+    for (var j = 0, k = 0; j < dataCount; j++, k++) {
+        if (j !== dataCount - 1) {
+            if (k === decimation) {
+                k = 0;
+            }
+            if (k !== 0) {
+                continue;
+            }
+        }
+
+        const data = dataList[j];
+        const timestamp = data.sent;
+        const time = timestamp.seconds * 1000 + timestamp.microseconds / 1000;
+
+        const deltaTime = (time - latestTime) / 1000.0;
+
+        for (var i = 0; i < fieldCount; i++) {
+            const field = data.payload.fields[i];
+            const fieldType = field.type;
+
+            if (fieldType === 'number') {
+
+                const fieldKey = sourceKey + '_' + i;
+                const fieldValue = field.value;
+
+                const config = g_chartConfigs[fieldKey];
+
+                config.data.labels.push(deltaTime.toFixed(1));
+                config.data.datasets[0].data.push(fieldValue);
+            }
+        }
+    }
+
+    // Update.
+    for (var i = 0; i < fieldCount; i++) {
+        const field = latestData.payload.fields[i];
+        const fieldType = field.type;
+        if (fieldType === 'number') {
+            const fieldKey = sourceKey + '_' + i;
+            const chart = g_charts[fieldKey];
+            chart.update();
+        }
+    }
+}
